dafe196c10cd5ca584bf33101bef6e1b30662643
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-06-04 16:57:50 +0200

    chore: Add form validation and styling for RF and TDF components

--- src/app/pages/rf/rf.component.ts
+++ src/app/pages/rf/rf.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rf',
   templateUrl: './rf.component.html',
   styleUrl: './rf.component.scss',
 })
-export class RfComponent {
+export class RfComponent implements OnInit {
   generi: string[] = ['uomo', 'donna', 'altro'];
   form!: FormGroup;
   constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      nome: this.fb.control(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      cognome: this.fb.control(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+
+      password: this.fb.control(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      passwordConfirm: this.fb.control(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      genere: this.fb.control(null),
+      profileImage: this.fb.control(null),
+      biografia: this.fb.control(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      nickname: this.fb.control(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    });
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.form.get(controlName);
+    return control?.invalid && control?.touched;
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+
+  autoFillForm() {
+    const datiDaFillare = {
+      nome: 'Gianmarco',
+      cognome: 'Tocco',
+      password: '123456',
+      passwordConfirm: '123456',
+      username: 'TmBlurr',
+    };
+    this.form.patchValue(datiDaFillare);
+  }
 }
--- src/app/pages/rf/rf.module.ts
+++ src/app/pages/rf/rf.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RfRoutingModule } from './rf-routing.module';
 import { RfComponent } from './rf.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RfComponent],
-  imports: [CommonModule, RfRoutingModule],
+  imports: [CommonModule, RfRoutingModule, ReactiveFormsModule],
 })
 export class RfModule {}
